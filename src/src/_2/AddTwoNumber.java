1da0268b253b98f220ee64377e009c410e34fdc5
lakhara <siddharth.lakhara@gmail.com>
2022-06-23 19:56:46 +0530

    soln 2 optimisation

--- src/src/_2/AddTwoNumber.java
+++ src/src/_2/AddTwoNumber.java
@@ -2,78 +2,26 @@ package _2;
 
 public class AddTwoNumber {
     private ListNode solve(ListNode l1, ListNode l2) {
-        ListNode resultsHead = null;
+        ListNode resultsHead = new ListNode();
         ListNode resultsRef = resultsHead;
-
         int carry = 0;
 
-        ListNode l1Ref = l1;
-        ListNode l2Ref = l2;
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = l1 != null ? l1.val : 0;
+            int val2 = l2 != null ? l2.val : 0;
 
-        while (l1Ref != null && l2Ref != null) {
-            int newVal = l1Ref.val + l2Ref.val + carry;
-            if (newVal >= 10) {
-                newVal %= 10;
-                carry = 1;
-            } else {
-                carry = 0;
-            }
+            int newVal = val1 + val2 + carry;
+            carry = Math.floorDiv(newVal, 10);
+            newVal %= 10;
 
-            if (resultsHead == null) {
-                resultsHead = new ListNode(newVal);
-                resultsRef = resultsHead;
-            } else {
-                ListNode newNode = new ListNode(newVal);
-                resultsRef.next = newNode;
-                resultsRef = newNode;
-            }
+            resultsRef.next = new ListNode(newVal);
 
-            l1Ref = l1Ref.next;
-            l2Ref = l2Ref.next;
+            l1 = l1 != null ? l1.next : l1;
+            l2 = l2 != null ? l2.next : l2;
+            resultsRef = resultsRef.next;
         }
 
-//        One of the list is not yet over
-        if (l1Ref != null || l2Ref != null) {
-            if (l1Ref == null) {
-                while (l2Ref != null) {
-                    int newVal = l2Ref.val + carry;
-                    if (newVal >= 10) {
-                        newVal %= 10;
-                        carry = 1;
-                    } else {
-                        carry = 0;
-                    }
-
-                    ListNode newNode = new ListNode(newVal);
-                    resultsRef.next = newNode;
-                    resultsRef = newNode;
-
-                    l2Ref = l2Ref.next;
-                }
-            } else {
-                while (l1Ref != null) {
-                    int newVal = l1Ref.val + carry;
-                    if (newVal >= 10) {
-                        newVal %= 10;
-                        carry = 1;
-                    } else {
-                        carry = 0;
-                    }
-
-                    ListNode newNode = new ListNode(newVal);
-                    resultsRef.next = newNode;
-                    resultsRef = newNode;
-
-                    l1Ref = l1Ref.next;
-                }
-            }
-        }
-
-        if (carry > 0) {
-            resultsRef.next = new ListNode(carry);
-        }
-
-        return resultsHead;
+        return resultsHead.next;
     }
 
     public void driver() {
