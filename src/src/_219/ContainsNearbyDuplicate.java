3837a187e990189647a3d88d089b6cb98fe7baf6
lakhara <siddharth.lakhara@gmail.com>
2022-07-19 17:41:49 +0530

    optimise soln 219

--- src/src/_219/ContainsNearbyDuplicate.java
+++ src/src/_219/ContainsNearbyDuplicate.java
@@ -1,26 +1,33 @@
 package _219;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class ContainsNearbyDuplicate {
 
     private boolean solve(int[] nums, int k) {
-        HashMap<Integer, Integer> keyMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (keyMap.containsKey(nums[i])) {
-                if ((i - keyMap.get(nums[i]) <= k)) {
-                    return true;
-                }
-            }
-            keyMap.put(nums[i], i);
+        HashSet<Integer> keySet = new HashSet<>();
 
+        k = Math.min(nums.length-1, k);
+        for (int i=0; i<=k; i++) {
+            keySet.add(nums[i]);
+        }
+        if (keySet.size() != k+1) {
+            return true;
+        }
+
+        for (int i = 1; i < nums.length-k; i++) {
+            keySet.remove(nums[i-1]);
+            keySet.add(nums[i+k]);
+            if (keySet.size() != k+1) {
+                return true;
+            }
         }
         return false;
     }
 
     public void driver() {
         int[] nums = new int[]{1,2,3,1};
-        int k = 3;
+        int k = 1;
         boolean answer = this.solve(nums, k);
         System.out.println(answer);
     }
